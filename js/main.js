4bfdc9fc62cfe2cb890ec0d15ebfff7f7da74b54
Eugeny Kiselyov <ekiselyov@me.com>
2015-06-25 19:45:28 +0300

    undefine commit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
+//Selectize
+$('.my-select').selectize();
 
 
 // Slider Counter
-+function($) {
-    var totalItems = $('#carousel-pidmoga .item').length;
-    var currentIndex = $('div.active').index() + 1;
-    $('span.slider-counter').html('' + currentIndex + '/' + totalItems + '');
-    $('#carousel-pidmoga').on('slid.bs.carousel', function() {
-        currentIndex = $('div.active').index() + 1;
-        $('span.slider-counter').html('' + currentIndex + '/' + totalItems + '');
-    });
-}(jQuery);
-
-//Selectize
-$('.my-select').selectize();
+// +function($) {
+//     var totalItems = $('#carousel-pidmoga .item').length;
+//     var currentIndex = $('div.active').index() + 1;
+//     $('span.slider-counter').html('' + currentIndex + '/' + totalItems + '');
+//     $('#carousel-pidmoga').on('slid.bs.carousel', function() {
+//         currentIndex = $('div.active').index() + 1;
+//         $('span.slider-counter').html('' + currentIndex + '/' + totalItems + '');
+//     });
+// }(jQuery);
