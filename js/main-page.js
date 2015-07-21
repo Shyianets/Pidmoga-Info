0272b9c2c97545402e1e07139ef2b682bcd4b893
Eugeny Kiselyov <ekiselyov@me.com>
2015-07-22 00:53:54 +0300

    add new page, improve menu

--- js/main-page.js
+++ js/main-page.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
 
 	$(document).on('click', '.main-page-container .header-menu-block .header_menu, .header .header-menu-block .header_menu, .fixnav .fix_menu', function(){
+        $('.under-menu').css('opacity','');
 		$('.top-menu-block').addClass('active');
+        $('.under-menu').addClass('under-menu-active');
     });
 
-    $(document).on('click', '.top-menu-block .menu-close', function(){
+    $(document).on('click', '.top-menu-block .menu-close, .under-menu', function(){
 		$('.top-menu-block').removeClass('active');
+        $('.under-menu').css('opacity', '0');
+        setTimeout(function() {
+            $('.under-menu').removeClass('under-menu-active');
+        }, 500);
     });
 
 })
